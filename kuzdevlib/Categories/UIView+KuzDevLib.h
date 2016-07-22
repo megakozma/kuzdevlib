260fae045e988ab8feb614a5dec1f4ebb867b892
Mikhail Kuzevanov <megakozma@gmail.com>
2016-07-22 13:19:43 +0500

    first commit

---
+++ kuzdevlib/Categories/UIView+KuzDevLib.h
@@ -0,0 +1,13 @@
+//
+//  UIView+KuzDevLib.h
+//  kuzdevlib
+//
+//  Created by Михаил Кузеванов on 22.07.16.
+//  Copyright © 2016 kuz-dev.ru. All rights reserved.
+//
+
+
+
+@interface UIView (KuzDevLib)
+
+@end
---
+++ kuzdevlib/Categories/UIView+KuzDevLib.m
@@ -0,0 +1,13 @@
+//
+//  UIView+KuzDevLib.m
+//  kuzdevlib
+//
+//  Created by Михаил Кузеванов on 22.07.16.
+//  Copyright © 2016 kuz-dev.ru. All rights reserved.
+//
+
+#import "UIView+KuzDevLib.h"
+
+@implementation UIView (KuzDevLib)
+
+@end
