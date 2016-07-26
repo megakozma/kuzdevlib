58cb81e5fd37960dfbb4edaea7a03648d2c7cb15
Mikhail Kuzevanov <megakozma@gmail.com>
2016-07-26 19:17:09 +0500

    1.0.10

--- kuzdevlib/KuzDev.h
+++ kuzdevlib/KuzDev.h
@@ -6,6 +6,20 @@
 //  Copyright © 2016 kuz-dev.ru. All rights reserved.
 //
 
+
+//RGB color macro
+#define UIColorFromRGB(rgbValue) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+//RGB color macro with alpha
+#define UIColorFromRGBWithAlpha(rgbValue,a) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
+
+
 #import "UIView+KuzDevLib.h"
 #import "UIButton+KuzDevLib.h"
 #import "UIAlertView+NSCookbook.h"
