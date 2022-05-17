4283a67cd8ad2f3d859622470e6c916e5766edf3
kozma <megakozma@gmail.com>
2022-05-17 12:25:10 +0500

    close helper

--- kuzdevlib.podspec
+++ kuzdevlib.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
 s.name         = "kuzdevlib"
-s.version      = "1.5.4"
+s.version      = "1.5.5"
 s.summary      = "Categories and Helpers."
 s.description  = <<-DESC
 Categories and Helpers for my development
---
+++ kuzdevlib/categories/Swift/KDLSwipeCloseHelper.swift
@@ -0,0 +1,68 @@
+//
+//  KDLSwipeCloseHelper.swift
+//  kuzdevlib
+//
+//  Created by Mikhail Kuzevanov on 17.05.2022.
+//  Copyright © 2022 kuz-dev.ru. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+typealias KDLVoidEvent = ()->Void
+
+class KDLSwipeCloseHelper {
+    
+    private var view: UIView? = nil
+    private var dismiss: KDLVoidEvent? = nil
+    private var bottomConstraint: NSLayoutConstraint?
+    
+    
+    func setToView(view: UIView, bottomConstraint: NSLayoutConstraint?, dismiss: KDLVoidEvent?){
+        self.view = view
+        self.dismiss = dismiss
+        self.bottomConstraint = bottomConstraint
+        let gest = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerHandler(_:)))
+        view.addGestureRecognizer(gest)
+    }
+    
+    private var initialTouchPoint: CGPoint = CGPoint(x: 0,y: 0)
+    private var prevTouchPoint: CGPoint = CGPoint(x: 0,y: 0)
+    private var initialBottomValue: CGFloat = 0
+    private var movingDown = false
+    
+    @objc private func panGestureRecognizerHandler(_ sender: UIPanGestureRecognizer) {
+        guard let view = view,
+              let superview = view.superview,
+              let bottomConstraint = bottomConstraint else { return  }
+        let touchPoint = sender.location(in: superview)
+        
+        if sender.state == .began {
+            initialTouchPoint = touchPoint
+            prevTouchPoint = touchPoint
+            initialBottomValue = bottomConstraint.constant
+        } else if sender.state == .changed {
+            let indent = touchPoint.y - initialTouchPoint.y
+            movingDown = touchPoint.y > prevTouchPoint.y
+            bottomConstraint.constant = initialBottomValue+indent
+            prevTouchPoint = touchPoint
+            
+        } else if sender.state == .ended || sender.state == .cancelled {
+            if movingDown{
+                bottomConstraint.constant = view.bounds.height
+                UIView.animate(withDuration: 0.3) {
+                    superview.layoutIfNeeded()
+                } completion: { (_) in
+                    if let dismiss = self.dismiss{
+                        dismiss()
+                    }
+                }
+            }
+            else{
+                bottomConstraint.constant = initialBottomValue
+                superview.layoutIfNeeded()
+            }
+
+        }
+    }
+}
